0cc104995c1c37892f602a05c572bb71f3533c85
seungbin.kim <seungbin.kim.dev@gmail.com>
2024-03-03 21:37:57 +0900

    프로듀서 메세지 전송 테스트

---
+++ producers/src/main/java/com/example/kafka/SimpleProducer.java
@@ -0,0 +1,38 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class SimpleProducer {
+
+    public static void main(String[] args) {
+
+        String topicName = "simple-topic";
+
+        // KafkaProducer configuration setting
+
+        Properties props = new Properties();
+
+        // bootstrap.servers, key.serializer.class, value.serializer.class
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // org.apache.kafka.common.serialization.StringSerializer
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // KafkaProducer object creation
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+
+        // ProducerRecord object creation
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, "hello world2");
+
+        // KafkaProducer message send
+        kafkaProducer.send(producerRecord);
+
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
+
+}
