b2ea428ba3e844b52120199747e931257f542a42
seungbin.kim <seungbin.kim.dev@gmail.com>
2024-03-09 16:12:38 +0900

    Add: Consumer 테스트

---
+++ consumers/src/main/java/com/example/kafka/SimpleConsumer.java
@@ -0,0 +1,48 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class SimpleConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(SimpleConsumer.class);
+
+    public static void main(String[] args) {
+
+        String topicName = "simple-topic";
+
+        // 기본적인 설정
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
+
+        try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props)) {
+            kafkaConsumer.subscribe(List.of(topicName));
+
+            while (true) {
+                // 브로커나 Consumer 내부 Queue에 데이터가 있다면 바로 데이터 반환
+                // 없다면 최대 Duration 동안 Fetch를 계속 Borker에 수행
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(1000));
+
+                for (ConsumerRecord<String, String> record : records) {
+
+                    logger.info("record key:{}, record value:{}, partition:{}",
+                            record.key(), record.value(), record.partition());
+                }
+            }
+
+        }
+    }
+
+}
