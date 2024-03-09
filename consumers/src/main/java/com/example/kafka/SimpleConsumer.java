904cff18727196b322f30c21f19cb33bc507deec
seungbin.kim <seungbin.kim.dev@gmail.com>
2024-03-09 20:16:22 +0900

    Modify: auto.offset.reset 설정 변경

--- consumers/src/main/java/com/example/kafka/SimpleConsumer.java
+++ consumers/src/main/java/com/example/kafka/SimpleConsumer.java
@@ -27,6 +27,8 @@ public class SimpleConsumer {
         props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
 
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
         try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props)) {
             kafkaConsumer.subscribe(List.of(topicName));
 
@@ -37,8 +39,8 @@ public class SimpleConsumer {
 
                 for (ConsumerRecord<String, String> record : records) {
 
-                    logger.info("record key:{}, record value:{}, partition:{}",
-                            record.key(), record.value(), record.partition());
+                    logger.info("record key:{}, record value:{}, partition:{}, record offset:{}",
+                            record.key(), record.value(), record.partition(), record.offset());
                 }
             }
 
